9b96eaf27c42b8d4bc0aa2646c327ccbaafc5a3f
28-shravya <66370715+28-shravya@users.noreply.github.com>
2021-04-12 15:11:42 +0530

    Added features

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -1,15 +1,14 @@
 /** 
 * @file calculator_operations.h
-* Calculator application with 4 mathematical operations
+* Calculator application with 3 modes of operations
 *
 */
-#ifndef __CALCULATOR_OPERATIONS_H__
-#define __CALCULATOR_OPERATIONS_H__
+#ifndef __CALCULATOR_MODES_OF_OPERATIONS_H__
+#define __CALCULATOR_MODES_OF_OPERATIONS_H__
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio_ext.h>
-
+#include<math.h>
 /**
 *  adds the operand1 and operand2 and returns the result
 * @param[in] operand1 
@@ -34,7 +33,6 @@ int subtract(int operand1, int operand2);
 */
 int multiply(int operand1, int operand2);
 
-
 /**
 * divides the operand1 by operand2 and returns the result 
 * @param[in] operand1 
@@ -43,5 +41,62 @@ int multiply(int operand1, int operand2);
 * @note returns 0 for divide by 0 error
 */
 int divide(int operand1, int operand2);
+
+/**
+* finds simple interest by formula and returns the result
+* @param[in] principal_amount 
+* @param[in] time_of_period 
+* @param[in] rate_of_interest
+* @return float value of the (principal_amount*time_of_period*rate_of_interest)/100
+*/
+float simple_interest(int principal_amount, int time_of_period, int rate_of_interest);
+
+/**
+* finds compound interest by formula and returns the result
+* @param[in] principal_amount 
+* @param[in] time_of_period 
+* @param[in] rate_of_interest
+* @param[in] no_of_applied_interst
+* @return float value of the principal_amount*(1+(rate_of_interest/no_of_applied_interest))^time_of_period*no_of_applied_interest
+*/
+
+float compound_interest(int principal_amount, int time_of_period, int rate_of_interest, int no_of_applied_interest);
+
+/**
+* finds total amount by formula and returns the result
+* @param[in] principal_amount 
+* @param[in] interest_amount 
+* @return integer value of the principal_amount+interest_amount
+*/
+int total_amount(int principal_amount, int interest_amount);
+
+/**
+*  finds the factorial of operand1 and returns the result
+* @param[in] operand1  
+* @return Result of operand1!
+*/
+int fact(int operand1);
+
+/**
+*  finds the operand1 to the power of operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1^operand2
+*/
+int power(int operand1, int operand2);
+
+/**
+*  finds the log of of operand1 to the base 10 and returns the result
+* @param[in] operand1  
+* @return Result of log(operand1)
+*/
+double logarithm(int operand1);
+
+/**
+*  finds the exponential to the power of operand1 and returns the result
+* @param[in] operand1  
+* @return Result of e^operand1
+*/
+double exponential(int operand1);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -22,3 +22,40 @@ int divide(int operand1, int operand2)
     else
         return operand1 / operand2;
 }
+
+float simple_interest(int principal_amount, int time_of_period, int rate_of_interest){
+    return (principal_amount*time_of_period*rate_of_interest)/100;
+}
+
+float compound_interest(int principal_amount, int time_of_period, int rate_of_interest, int no_of_applied_interest)
+{
+    return principal_amount*((1+(rate_of_interest/no_of_applied_interest))^(time_of_period*no_of_applied_interest));
+}
+
+int total_amount(int principal_amount, int interest_amount){
+    return principal_amount+interest_amount;
+}
+
+int fact(int operand1){
+    int i,f=1;
+    if (operand1<1)
+    {
+        return 1;
+    }
+    for(i=1;i<=operand1;i++){
+    f=f*i;
+    }
+    return f;
+}
+
+int power(int operand1, int operand2){
+    return pow(operand1,operand2);
+}
+
+double logarithm(int operand1){
+    return log10(operand1);
+}
+
+double exponential(int operand1){
+    return exp(operand1);
+}
