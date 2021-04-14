666b560e49ce21bfd7fe9372f27a8894f531fcf2
Shravya K N <66370715+28-shravya@users.noreply.github.com>
2021-04-14 17:19:51 +0530

    Update calculator_operations.h

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -49,7 +49,7 @@ int divide(int operand1, int operand2);
 * @param[in] rate_of_interest
 * @return float value of the (principal_amount*time_of_period*rate_of_interest)/100
 */
-float simple_interest(int principal_amount, int time_of_period, int rate_of_interest);
+double simple_interest(double principal_amount, double time_of_period, double rate_of_interest);
 
 /**
 * finds compound interest by formula and returns the result
@@ -57,10 +57,9 @@ float simple_interest(int principal_amount, int time_of_period, int rate_of_inte
 * @param[in] time_of_period 
 * @param[in] rate_of_interest
 * @param[in] no_of_applied_interst
-* @return float value of the principal_amount*(1+(rate_of_interest/no_of_applied_interest))^time_of_period*no_of_applied_interest
+* @return float value of the principal_amount*(1+(rate_of_interest/100))^time_of_period
 */
-
-float compound_interest(int principal_amount, int time_of_period, int rate_of_interest, int no_of_applied_interest);
+double compound_interest(double principal_amount, double time_of_period, double rate_of_interest);
 
 /**
 * finds total amount by formula and returns the result
@@ -68,14 +67,14 @@ float compound_interest(int principal_amount, int time_of_period, int rate_of_in
 * @param[in] interest_amount 
 * @return integer value of the principal_amount+interest_amount
 */
-int total_amount(int principal_amount, int interest_amount);
+double total_amount(double principal_amount, double interest_amount);
 
 /**
 *  finds the factorial of operand1 and returns the result
 * @param[in] operand1  
 * @return Result of operand1!
 */
-int fact(int operand1);
+double fact(int operand1);
 
 /**
 *  finds the operand1 to the power of operand2 and returns the result
@@ -83,20 +82,20 @@ int fact(int operand1);
 * @param[in] operand2 
 * @return Result of operand1^operand2
 */
-int power(int operand1, int operand2);
+double power(double operand1, double operand2);
 
 /**
 *  finds the log of of operand1 to the base 10 and returns the result
 * @param[in] operand1  
 * @return Result of log(operand1)
 */
-double logarithm(int operand1);
+double logarithm(double operand1);
 
 /**
 *  finds the exponential to the power of operand1 and returns the result
 * @param[in] operand1  
 * @return Result of e^operand1
 */
-double exponential(int operand1);
+double exponential(double operand1);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
